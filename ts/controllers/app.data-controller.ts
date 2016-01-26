978eccc66ae228adbbaa06557ba2ec2b43eb2e4f
Phil Coltrane <phil.m.coltrane@gmail.com>
2016-01-25 22:52:33 -0500

    filter data items by various fields

--- ts/controllers/app.data-controller.ts
+++ ts/controllers/app.data-controller.ts
@@ -19,7 +19,7 @@ module MTConnectViewer {
                     category: item.getAttribute('category'),
                     type: item.getAttribute('type'),
                     id: item.getAttribute('id'),
-                    
+
                     path: []
                 };
 
@@ -28,25 +28,26 @@ module MTConnectViewer {
                 if (item.hasAttribute('units')) workingItem.units = item.getAttribute('units');
 
                 this.dataItems.push(workingItem);
-                
-                while(!!item.parentElement.parentElement){
+
+                while (!!item.parentElement.parentElement) {
                     item = item.parentElement.parentElement;
                     var componentParent = item.parentElement;
-                    
+
                     var component: IComponent = {
                         id: item.getAttribute('id'),
                         type: item.tagName
                     }
-                    if(item.hasAttribute('name')) component.name = item.getAttribute('name');
+                    if (item.hasAttribute('name')) component.name = item.getAttribute('name');
                     workingItem.path.unshift(component);
-                    
-                    if(componentParent.tagName !== 'Components') break;
+
+                    if (componentParent.tagName !== 'Components') break;
                 }
             }
         }
 
         public devicesDocument: Document;
         public dataItems: IDataItem[] = [];
+        public filter: string;
 
         public constructor(private $state: angular.ui.IStateService, private agent: IAgent, private $scope: angular.IScope, private $timeout: angular.ITimeoutService, private $interval: angular.IIntervalService) {
             agent.devices.then(devices => {
@@ -55,66 +56,65 @@ module MTConnectViewer {
                 $timeout(() => {
                     console.log('processed', this.dataItems.length, 'dataItems');
                 }, 0);
-                
+
                 this._updateInterval = $interval(this.fetch, 10000);
-                
+
                 $scope.$on('$destroy', () => {
-                   $interval.cancel(this._updateInterval); 
+                    $interval.cancel(this._updateInterval);
                 });
-                
+
                 this.fetch();
             });
-            
-            
+
+
         }
 
         public viewDevice = (name: string) => {
             this.$state.go('device', { device: name });
         }
-        
+
         public fetch = () => {
             this.agent.current.then(dataitems => {
                 var streams = dataitems.getElementsByTagName('ComponentStream');
-                var values: {[id:string]:any} = {};
-                var conditions: {[id:string]:any} = {};
-                for(var i=0; i<streams.length; i++){
+                var values: { [id: string]: any } = {};
+                var conditions: { [id: string]: any } = {};
+                for (var i = 0; i < streams.length; i++) {
                     var stream = streams.item(i);
-                    
+
                     var samples = stream.getElementsByTagName('Samples');
                     var events = stream.getElementsByTagName('Events');
                     var condition = stream.getElementsByTagName('Condition');
-                    
-                    for(var j=0; j<samples.length; j++){
+
+                    for (var j = 0; j < samples.length; j++) {
                         var items = samples.item(j).childNodes;
-                        for(var k=0; k<items.length; k++){
-                            if(!(<Element>items.item(k)).getAttribute) continue;
+                        for (var k = 0; k < items.length; k++) {
+                            if (!(<Element>items.item(k)).getAttribute) continue;
                             values[(<Element>items.item(k)).getAttribute('dataItemId')] = items.item(k).textContent;
                         }
                     }
-                    for(var j=0; j<events.length; j++){
+                    for (var j = 0; j < events.length; j++) {
                         var items = events.item(j).childNodes;
-                        for(var k=0; k<items.length; k++){
-                            if(!(<Element>items.item(k)).getAttribute) continue;
+                        for (var k = 0; k < items.length; k++) {
+                            if (!(<Element>items.item(k)).getAttribute) continue;
                             values[(<Element>items.item(k)).getAttribute('dataItemId')] = items.item(k).textContent;
                         }
                     }
-                    for(var j=0; j<condition.length; j++){
+                    for (var j = 0; j < condition.length; j++) {
                         var items = condition.item(j).childNodes;
-                        for(var k=0; k<items.length; k++){
-                            if(!(<Element>items.item(k)).getAttribute) continue;
+                        for (var k = 0; k < items.length; k++) {
+                            if (!(<Element>items.item(k)).getAttribute) continue;
                             values[(<Element>items.item(k)).getAttribute('dataItemId')] = items.item(k).textContent;
                             conditions[(<Element>items.item(k)).getAttribute('dataItemId')] = (<Element>items.item(k)).tagName;
                         }
                     }
                 }
-                
-                for(var i=0; i<this.dataItems.length; i++){
+
+                for (var i = 0; i < this.dataItems.length; i++) {
                     var dataitem = this.dataItems[i];
-                    if(values.hasOwnProperty(dataitem.id)){
+                    if (values.hasOwnProperty(dataitem.id)) {
                         dataitem.value = values[dataitem.id];
-                        console.log(dataitem.id, ' == ', dataitem.value);
                     }
-                    if(conditions.hasOwnProperty(dataitem.id)) dataitem.condition = conditions[dataitem.id];
+                    if (conditions.hasOwnProperty(dataitem.id)) dataitem.condition = conditions[dataitem.id];
                 }
                 this.$timeout(() => {
                     console.log('processed current dataItems');
@@ -122,7 +122,26 @@ module MTConnectViewer {
             });
         }
 
+        public isFiltered = (dataitem: IDataItem): boolean => {
+            if (!this.filter || this.filter.length === 0) return true;
+
+            var regex = new RegExp('^' + this.filter, 'i');
+            if (regex.exec(dataitem.name || dataitem.id)) return true;
+            if(regex.exec(dataitem.type)) return true;
+            if(regex.exec(dataitem.subType)) return true;
+            
+            for(var i in dataitem.path) if(
+                regex.exec(dataitem.path[i].name) 
+                || regex.exec(dataitem.path[i].type) 
+                || regex.exec(dataitem.path[i].id)
+                ) return true;
+            
+            console.log('no match with ', dataitem.name);
+            return false;
+        }
+
     }
+
 
     MTConnectViewer.app.controller('dataCtrl', MTConnectViewer.DataController);
 }
