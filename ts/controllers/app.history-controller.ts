9d32e7431c50fef3ada9b830f795de686b74b0b4
Phil Coltrane <phil.m.coltrane@gmail.com>
2016-01-27 23:40:06 -0500

    basic history display (flat list of values)

--- ts/controllers/app.history-controller.ts
+++ ts/controllers/app.history-controller.ts
@@ -7,13 +7,17 @@ module MTConnectViewer {
         public static $inject = ['$state', 'agent', '$scope', '$timeout', '$interval'];
 
         public dataItemIds: string[];
+        public samples: {[name: string]: IDataItemSample[]};
         
         public constructor(private $state: angular.ui.IStateService, private agent: IAgent, private $scope: angular.IScope, private $timeout: angular.ITimeoutService, private $interval: angular.IIntervalService) {
             this.dataItemIds = this.$state.params['id'].split(',');
             
             this.agent.getSamples(this.dataItemIds)
             .then(result => {
-               console.log(result); 
+               this.samples = result;
+               this.$timeout(() => {
+                   console.log('processed ' + Object.keys(this.samples).length + ' dataitems');
+               }, 0);
             });
         }
 
